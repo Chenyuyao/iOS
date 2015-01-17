73d0f9da90e8ffefda8e088a04e1a538ffe21a4f
Frank Li <frankli@v1020-wn-194-31.campus-dynamic.uwaterloo.ca>
2015-01-17 15:59:40 -0500

    Initial Commit

---
+++ motcha/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  motcha
+//
+//  Created by Frank Li on 1/17/15.
+//  Copyright (c) 2015 Frank Li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ motcha/MasterViewController.h
@@ -0,0 +1,19 @@
+//
+//  MasterViewController.h
+//  motcha
+//
+//  Created by Frank Li on 1/17/15.
+//  Copyright (c) 2015 Frank Li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+
+@end
+
