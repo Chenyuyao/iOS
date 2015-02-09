418bcff5719f374c7d54027ff955db961cc18dfe
Frank Li <frankvsense@gmail.com>
2015-02-09 12:03:15 -0500

    Add documentation for MCKeyValuePair.

--- motcha/DataStructure/MCKeyValuePair.h
+++ motcha/DataStructure/MCKeyValuePair.h
@@ -1,5 +1,9 @@
 #import <Foundation/Foundation.h>
 
+/**
+ Objective-C does not natively provide a key-value-pair-like structure, so we implement one on our own,
+ in case for future use.
+ */
 @interface MCKeyValuePair : NSObject<NSCopying>
 @property (nonatomic, strong) id key;
 @property (nonatomic, strong) id value;
